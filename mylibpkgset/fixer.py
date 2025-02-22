a8a32ac8703f7d8086bf5d495aedec83d3c395a5
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-22 14:38:36 +0900

    unm fixer.py

--- mylibpkgset/fixer.py
+++ mylibpkgset/fixer.py
@@ -1,8 +1,11 @@
-from martialaw.martialaw import martialaw as _closer
-from sys import argv as a
+from martialaw.martialaw import martialaw as _closr
+from sys import argv as _a
 from os.path import splitext as _s
 _o = open
-@_closer
+@_closr
 def fixer(fun, fil):
     with _o(f'{_s(fil}.py','w') as w:
         with _o(fil) as r:fun(w,r)
+@_closr
+def fixermain(fun, fil):
+    fixer(fun)(fil[1])
